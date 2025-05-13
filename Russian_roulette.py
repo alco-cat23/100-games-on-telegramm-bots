e160cab704ca23dbce6454b50776f4607295d88a
alco-cat23 <artemkurnasenko2010@gmail.com>
2025-05-13 16:41:56 +0300

    Add files via upload

---
+++ Russian_roulette.py
@@ -0,0 +1,25 @@
+import telebot
+import random
+
+
+bot = telebot.TeleBot('token')
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Кавказская рулетка, выбери пулю от /1 до /6')
+
+
+@bot.message_handler(commands=['1', '2', '3', '4', '5', '6'])
+def russian_roulette(message):
+    bullet_nums = ['1', '2', '3', '4', '5', '6']
+    bullet = random.choice(bullet_nums)
+    user_choice = message.text[1:]  # Убираем "/" из команды
+
+    if user_choice == bullet:
+        bot.send_message(message.chat.id, 'Ты умер, проиграл')
+    else:
+        bot.send_message(message.chat.id, 'Ты остался жив, выиграл')
+
+
+bot.infinity_polling()
